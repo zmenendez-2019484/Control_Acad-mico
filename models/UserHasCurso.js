935bae52359c0312999038512d5efced578f8419
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-02-24 19:57:02 -0600

    Se agrega el modelo de union entre user y curso

---
+++ models/UserHasCurso.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const UserHasCourseSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  course: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course',
+    required: true
+  }
+});
+
+module.exports = mongoose.model('UserHasCourse', UserHasCourseSchema);
