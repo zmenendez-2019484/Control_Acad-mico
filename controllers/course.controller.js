737745207fd2a65bfd362cdf2ac93384a0b83896
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-02-25 11:47:07 -0600

    Se termina funcionalidades de Profesor

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -54,7 +54,7 @@ const cursosPorEstudiante = async (req, res) => {
         const { userId } = req.params;
         const usuarioAutenticado = req.usuario;
 
-        // Verificar si el usuario autenticado es el mismo que intenta ver los cursos
+        // Verificar si el usuario es el mismo que intenta ver los cursos
         if (usuarioAutenticado._id.toString() !== userId || usuarioAutenticado.role !== 'STUDENT_ROLE') {
             return res.status(403).json({
                 msg: 'No tienes permiso para ver los cursos de este usuario',
@@ -64,7 +64,7 @@ const cursosPorEstudiante = async (req, res) => {
         // Buscar las asignaciones del alumno
         const assignments = await UserHasCourse.find({ user: userId });
 
-        // Extraer los IDs de los cursos
+        // Encontrar los IDs de los cursos
         const courseIds = assignments.map(assignment => assignment.course);
 
         // Buscar los detalles de los cursos
@@ -83,8 +83,142 @@ const cursosPorEstudiante = async (req, res) => {
     }
 };
 
+const postCourse = async (req, res) => {
+    try {
+        const { name, description } = req.body;
+        // Crear el curso
+        const course = new Course({
+            name,
+            description,
+            teacher: req.usuario._id
+        });
+        // Guardar el curso
+        await course.save();
+        res.status(201).json({
+            msg: 'Curso creado correctamente',
+            course
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador',
+        });
+    }
+};
+
+const putCourse = async (req, res) => {
+    try {
+        const { courseId } = req.params;
+        const { name, description } = req.body;
+
+        // Comprueba si el profesor es el propietario del curso
+        const course = await Course.findById(courseId);
+        if (!course) {
+            return res.status(404).json({
+                msg: 'Curso no encontrado',
+            });
+        }
+        if (course.teacher.toString() !== req.usuario._id.toString()) {
+            return res.status(403).json({
+                msg: 'No tienes permiso para editar este curso',
+            });
+        }
+
+        course.name = name;
+        course.description = description;
+        await course.save();
+
+        // Actualizar la información del curso para los alumnos asignados
+        await User.updateMany(
+            { 'courses.courseId': courseId },
+            { $set: { 'courses.$.name': name, 'courses.$.description': description } }
+        );
+
+        res.status(200).json({
+            msg: 'Curso editado correctamente',
+            course
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador',
+        });
+    }
+};
+
+const deleteCourse = async (req, res) => {
+    try {
+        const { courseId } = req.params;
+
+        // Comprueba si el profesor es el propietario del curso
+        const course = await Course.findById(courseId);
+        if (!course) {
+            return res.status(404).json({
+                msg: 'Curso no encontrado',
+            });
+        }
+        if (course.teacher.toString() !== req.usuario._id.toString()) {
+            return res.status(403).json({
+                msg: 'No tienes permiso para eliminar este curso',
+            });
+        }
+
+        // Cambiar el estado del curso a false
+        course.estado = false;
+        await course.save();
+
+        // Desasignar a los alumnos del curso
+        await User.updateMany(
+            { 'courses.courseId': courseId },
+            { $pull: { courses: { courseId: courseId } } }
+        );
+
+        res.status(200).json({
+            msg: 'Curso eliminado correctamente',
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador',
+        });
+    }
+};
+
+const cursosDeProfesor = async (req, res) => {
+    try {
+        // Buscar todos los cursos donde el profesor es el creador del curso y el estado es true
+        const courses = await Course.find({
+            teacher: req.usuario._id,
+            estado: true
+        });
+
+        // Verificar si hay cursos disponibles
+        if (courses.length ===  0) {
+            return res.status(200).json({
+                msg: 'No tienes cursos disponibles',
+                courses: []
+            });
+        }
+
+        res.status(200).json({
+            msg: 'Cursos obtenidos correctamente',
+            courses
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador',
+        });
+    }
+};
+
+
 
 module.exports = {
     asignarCursoAEstudiante,
-    cursosPorEstudiante
+    cursosPorEstudiante,
+    postCourse,
+    putCourse,
+    deleteCourse,
+    cursosDeProfesor
 };
--- routes/course.routes.js
+++ routes/course.routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { Router } = require('express');
 const { tieneRole } = require('../middlewares/validar-roles');
 const { check } = require('express-validator');
-const { asignarCursoAEstudiante, cursosPorEstudiante } = require('../controllers/course.controller');
+const { asignarCursoAEstudiante, cursosPorEstudiante, deleteCourse, postCourse, putCourse
+    , cursosDeProfesor } = require('../controllers/course.controller');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 
@@ -21,6 +22,32 @@ router.post('/students/:userId/courses/:courseId', [
     check('courseId', 'El ID del curso es requerido').notEmpty(),
     validarCampos,
     asignarCursoAEstudiante
+]);
+
+router.post('/', [
+    validarJWT,
+    tieneRole('TEACHER_ROLE'),
+    validarCampos,
+    postCourse
+]);
+
+router.put('/:courseId', [
+    validarJWT,
+    tieneRole('TEACHER_ROLE'),
+    validarCampos,
+    putCourse
+]);
+
+router.delete('/:courseId', [
+    validarJWT,
+    tieneRole('TEACHER_ROLE'),
+    deleteCourse
+]);
+
+router.get('/teachers/:teacherId/courses', [
+    validarJWT,
+    tieneRole('TEACHER_ROLE'),
+    cursosDeProfesor
 ]);
 
 module.exports = router;
